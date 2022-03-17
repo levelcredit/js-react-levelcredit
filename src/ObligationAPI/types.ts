825d3a4725050583c5a2d0ec8373c6f26ebd7019
Eugene Sodin <esodin@ukr.net>
2022-03-17 15:08:18 +0200

    RT-14191: Add transaction details to tradeline history

--- src/ObligationAPI/types.ts
+++ src/ObligationAPI/types.ts
@@ -1,5 +1,5 @@
 export type ObligationTradelineObject = { [key: string]: TradelineStatusObject };
-export type TradelineStatusObject = { status: TradelineStatus };
+export type TradelineStatusObject = { status: TradelineStatus; transaction_details: TransactionDetailsItem[] };
 export type TradelineStatus =
   | "POSITIVE_REPORT"
   | "POSITIVE_LOOKBACK_REPORT"
@@ -13,7 +13,7 @@ export type TradelineStatus =
   | "NEGATIVE_LATE_150"
   | "NEGATIVE_LATE_180"
   | "NO_PAYMENT";
-
+export type TransactionDetailsItem = { date: Date; amount: number; name: string; status: string | null };
 export type TradelineStringDateObject = { year: number | null; month: number | null };
 export type TradelineDateRange = { start_date: TradelineStringDateObject; end_date: TradelineStringDateObject };
 export type TradelineHistoryDataStyle = "last-24-months" | "last-24-datasets" | "all";
